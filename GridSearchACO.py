9a8f454e07cbfcbae8cdab23724bfe28471b53d9
Nhuan Tran <tranducnhuan1994@gmail.com>
2016-03-22 08:22:01 +0700

    Estimator Neural Net Model Fix Parallel Estimator ACO Params

--- GridSearchACO.py
+++ GridSearchACO.py
@@ -4,19 +4,23 @@ from MetricFeeder import MetricFeeder
 from ACOEstimator import ACOEstimator
 param_dicts = {
     "Q":np.arange(0.01,0.1,step=0.01),
-    "epsilon":np.arange(0.1,1.0,step=0.05)
+    "epsilon":np.arange(0.1,1.0,step=0.05),
+    "number_of_solutions":np.arange(30,200)
 }
-n_sliding_window = 4
-n_periodic = 1
-n_input = n_sliding_window + n_periodic
-neural_shape=[n_input,15,1]
+n_windows = 5
+n_hidden = 10
+range_training = (-1,28919)
+range_test = (28919,-1)
+metric_types = ["cpu_util","disk_write_rate"]
 dataFeeder = MetricFeeder()
-X,y = dataFeeder.fetch_traffic_training(n_sliding_window,n_periodic,(40,46))
+X_train,y_train = dataFeeder.fetch(metric_types,n_windows,range_training)
+
+neural_shape = [n_windows,n_hidden,2]
 estimator = ACOEstimator()
 archive_solution = construct_solution(estimator.number_of_solutions,neural_shape)
 fit_param = {'neural_shape':neural_shape,"archive":archive_solution}
 # estimator.fit(X,y,**fit_param)
-gs = GridSearchCV(estimator,param_grid=param_dicts,cv=3,n_jobs=-1,fit_params=fit_param,scoring='mean_squared_error')
-gs.fit(X,y)
+gs = GridSearchCV(estimator,param_grid=param_dicts,n_jobs=-1,fit_params=fit_param,scoring='mean_squared_error')
+gs.fit(X_train,y_train)
 print gs.best_estimator_
-print gs.best_estimator_.score(X,y)
+print gs.best_estimator_.score(X_train,y_train)
